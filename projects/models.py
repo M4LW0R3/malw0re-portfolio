1e293bce0a67511ebfed2741fbf95410e91d7359
Brian Karanja <youngkhalifah98@gmail.com>
2019-08-12 13:02:34 +0300

    deleted

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=70)
+    image = FilePathField(path="/img")

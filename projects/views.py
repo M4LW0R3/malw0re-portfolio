9f892727fb49827f8ce8411e5a4b8ccd1920e4ce
Brian Karanja <youngkhalifah98@gmail.com>
2019-08-12 13:19:25 +0300

    Models created

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from projects.models import Project
 # Create your views here.
+def project_showcase(request):
+    projects =Project.object.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_showcase.html', context)
